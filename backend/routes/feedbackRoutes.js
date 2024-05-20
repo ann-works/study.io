391008642b1ce5c631a75ad244485b8194001a55
ann-works <annesha947n@gmail.com>
2024-05-20 23:01:49 +0530

    first commit

---
+++ backend/routes/feedbackRoutes.js
@@ -0,0 +1,19 @@
+import {Router} from "express";
+import { Feedback } from "../models/feedbackModel.js";
+
+const router = Router()
+
+router.post("/feedback", async (req, res) => {
+  const feedback = new Feedback({
+    feedback: req.body.feedback,
+  });
+
+  try {
+    const savedFeedback = await feedback.save();
+    res.status(201).json(savedFeedback);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+export default router
